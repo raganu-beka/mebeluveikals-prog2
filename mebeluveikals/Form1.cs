a286be1b22f8e74380f31ff6eda4e912395e0cc9
datorium <datorium@BEKAS-PC>
2025-01-20 08:02:34 +0200

    Add project files.

---
+++ mebeluveikals/Form1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace mebeluveikals
+{
+    public partial class Form1 : Form
+    {
+        private FurnitureManager furnitureManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            furnitureManager = new FurnitureManager("Data Source=furniture.db");
+        }
+
+        private void selectBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(nameTextBox.Text))
+                {
+                    MessageBox.Show("Nav norādīts nosaukums.");
+                }
+                else if (string.IsNullOrEmpty(descTextBox.Text))
+                {
+                    MessageBox.Show("Nav norādīts apraksts.");
+                }
+                else if (string.IsNullOrEmpty(priceTextBox.Text))
+                {
+                    MessageBox.Show("Nav norādīta cena.");
+                }
+                else if (string.IsNullOrEmpty(hTextBox.Text))
+                {
+                    MessageBox.Show("Nav norādīts augstums.");
+                }
+                else if (string.IsNullOrEmpty(wTextBox.Text))
+                {
+                    MessageBox.Show("Nav norādīts platums.");
+                }
+                else if (string.IsNullOrEmpty(lTextBox.Text))
+                {
+                    MessageBox.Show("Nav norādīts garums.");
+                }
+
+
+                furnitureManager.AddFurniture(nameTextBox.Text, descTextBox.Text,
+                    Convert.ToDouble(priceTextBox.Text), Convert.ToInt32(hTextBox.Text),
+                    Convert.ToInt32(wTextBox.Text), Convert.ToInt32(lTextBox.Text));
+
+                MessageBox.Show("Ieraksts tika pievienots datubāzei");
+            }
+            catch (SqliteException ex)
+            {
+                MessageBox.Show("Notikusi SQL kļūda.");
+            } catch (Exception ex) {
+                MessageBox.Show("Notikusi kļūda.");
+            }
+        }
+    }
+}
---
+++ mebeluveikals/FurnitureManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+
+namespace mebeluveikals
+{
+    public class FurnitureManager
+    {
+        private readonly string connectionString;
+
+        public FurnitureManager(string connectionString)
+        {
+            this.connectionString = connectionString;
+
+            CreateFurnitureTable();
+        }
+
+        public void AddFurniture(string name, string description, double price,
+            int height, int width, int length)
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var createRecordCommand = connection.CreateCommand();
+                createRecordCommand.CommandText = @"INSERT INTO Furniture(Name, Description, Price, Height, Width, Length)
+                VALUES (@name, @description, @price, @height, @width, @length)";
+
+                createRecordCommand.Parameters.AddWithValue("name", name);
+                createRecordCommand.Parameters.AddWithValue("description", description);
+                createRecordCommand.Parameters.AddWithValue("price", price);
+                createRecordCommand.Parameters.AddWithValue("height", height);
+                createRecordCommand.Parameters.AddWithValue("width", width);
+                createRecordCommand.Parameters.AddWithValue("length", length);
+
+                createRecordCommand.ExecuteNonQuery();
+            }
+        }
+
+        private void CreateFurnitureTable()
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                var createTableCommand = connection.CreateCommand();
+                createTableCommand.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS Furniture (
+                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        Name TEXT NOT NULL UNIQUE,
+                        Description TEXT NOT NULL,
+                        Price REAL NOT NULL,
+                        Height INTEGER NOT NULL,
+                        Width INTEGER NOT NULL,
+                        Length INTEGER NOT NULL   
+                    ); 
+                ";
+                createTableCommand.ExecuteNonQuery();
+            }
+        }
+    }
+}
